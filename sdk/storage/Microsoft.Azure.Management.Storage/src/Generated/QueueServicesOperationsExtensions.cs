bc362104a5fd06d90597637a2b561cba13d56732
SDK Automation <sdkautomation@microsoft.com>
2020-05-11 16:43:18 +0000

    Generated from c52e6ae80429f2b945f6e13a083bfecdb695c164
    Change operationID for table and queue, update the name of an example

--- sdk/storage/Microsoft.Azure.Management.Storage/src/Generated/QueueServicesOperationsExtensions.cs
+++ sdk/storage/Microsoft.Azure.Management.Storage/src/Generated/QueueServicesOperationsExtensions.cs
@@ -22,6 +22,52 @@ namespace Microsoft.Azure.Management.Storage
     public static partial class QueueServicesOperationsExtensions
     {
             /// <summary>
+            /// List all queue services for the storage account
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='resourceGroupName'>
+            /// The name of the resource group within the user's subscription. The name is
+            /// case insensitive.
+            /// </param>
+            /// <param name='accountName'>
+            /// The name of the storage account within the specified resource group.
+            /// Storage account names must be between 3 and 24 characters in length and use
+            /// numbers and lower-case letters only.
+            /// </param>
+            public static ListQueueServices List(this IQueueServicesOperations operations, string resourceGroupName, string accountName)
+            {
+                return operations.ListAsync(resourceGroupName, accountName).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// List all queue services for the storage account
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='resourceGroupName'>
+            /// The name of the resource group within the user's subscription. The name is
+            /// case insensitive.
+            /// </param>
+            /// <param name='accountName'>
+            /// The name of the storage account within the specified resource group.
+            /// Storage account names must be between 3 and 24 characters in length and use
+            /// numbers and lower-case letters only.
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<ListQueueServices> ListAsync(this IQueueServicesOperations operations, string resourceGroupName, string accountName, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.ListWithHttpMessagesAsync(resourceGroupName, accountName, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
             /// Sets the properties of a storage account’s Queue service, including
             /// properties for Storage Analytics and CORS (Cross-Origin Resource Sharing)
             /// rules.
