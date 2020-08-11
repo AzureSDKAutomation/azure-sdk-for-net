9d159a5398ca0b29e9e15c232c3586f31a79571d
SDK Automation <sdkautomation@microsoft.com>
2020-08-11 06:51:37 +0000

    Update from master

--- sdk/maps/Microsoft.Azure.Management.Maps/src/generate.ps1
+++ sdk/maps/Microsoft.Azure.Management.Maps/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "maps/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "maps/resource-manager" -AutoRestVersion "latest"
