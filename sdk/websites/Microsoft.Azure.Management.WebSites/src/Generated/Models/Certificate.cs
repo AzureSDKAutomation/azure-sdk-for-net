8e1806b6ab63d23d8721bc5e1fcda1d811bb8582
SDKAuto <sdkautomation@microsoft.com>
2020-12-08 21:20:39 +0000

    CodeGen from PR 12014 in Azure/azure-rest-api-specs Merge 3db9067827c9e9d65d19b0dbf3c5c5d1ea51c067 into 1a7d8fa459198d9289981025e75482cd0c604a69

--- sdk/websites/Microsoft.Azure.Management.WebSites/src/Generated/Models/Certificate.cs
+++ sdk/websites/Microsoft.Azure.Management.WebSites/src/Generated/Models/Certificate.cs
@@ -35,12 +35,13 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// Initializes a new instance of the Certificate class.
         /// </summary>
         /// <param name="location">Resource Location.</param>
-        /// <param name="password">Certificate password.</param>
         /// <param name="id">Resource Id.</param>
         /// <param name="name">Resource Name.</param>
         /// <param name="kind">Kind of resource.</param>
         /// <param name="type">Resource type.</param>
         /// <param name="tags">Resource tags.</param>
+        /// <param name="systemData">The system metadata relating to this
+        /// resource.</param>
         /// <param name="friendlyName">Friendly name of the
         /// certificate.</param>
         /// <param name="subjectName">Subject name of the certificate.</param>
@@ -52,6 +53,7 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// <param name="issuer">Certificate issuer.</param>
         /// <param name="issueDate">Certificate issue Date.</param>
         /// <param name="expirationDate">Certificate expiration date.</param>
+        /// <param name="password">Certificate password.</param>
         /// <param name="thumbprint">Certificate thumbprint.</param>
         /// <param name="valid">Is the certificate valid?.</param>
         /// <param name="cerBlob">Raw bytes of .cer file</param>
@@ -72,8 +74,10 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// "/subscriptions/{subscriptionID}/resourceGroups/{groupName}/providers/Microsoft.Web/serverfarms/{appServicePlanName}".</param>
         /// <param name="canonicalName">CNAME of the certificate to be issued
         /// via free certificate</param>
-        public Certificate(string location, string password, string id = default(string), string name = default(string), string kind = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), string friendlyName = default(string), string subjectName = default(string), IList<string> hostNames = default(IList<string>), byte[] pfxBlob = default(byte[]), string siteName = default(string), string selfLink = default(string), string issuer = default(string), System.DateTime? issueDate = default(System.DateTime?), System.DateTime? expirationDate = default(System.DateTime?), string thumbprint = default(string), bool? valid = default(bool?), byte[] cerBlob = default(byte[]), string publicKeyHash = default(string), HostingEnvironmentProfile hostingEnvironmentProfile = default(HostingEnvironmentProfile), string keyVaultId = default(string), string keyVaultSecretName = default(string), KeyVaultSecretStatus? keyVaultSecretStatus = default(KeyVaultSecretStatus?), string serverFarmId = default(string), string canonicalName = default(string))
-            : base(location, id, name, kind, type, tags)
+        /// <param name="domainValidationMethod">Method of domain validation
+        /// for free cert</param>
+        public Certificate(string location, string id = default(string), string name = default(string), string kind = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), SystemData systemData = default(SystemData), string friendlyName = default(string), string subjectName = default(string), IList<string> hostNames = default(IList<string>), byte[] pfxBlob = default(byte[]), string siteName = default(string), string selfLink = default(string), string issuer = default(string), System.DateTime? issueDate = default(System.DateTime?), System.DateTime? expirationDate = default(System.DateTime?), string password = default(string), string thumbprint = default(string), bool? valid = default(bool?), byte[] cerBlob = default(byte[]), string publicKeyHash = default(string), HostingEnvironmentProfile hostingEnvironmentProfile = default(HostingEnvironmentProfile), string keyVaultId = default(string), string keyVaultSecretName = default(string), KeyVaultSecretStatus? keyVaultSecretStatus = default(KeyVaultSecretStatus?), string serverFarmId = default(string), string canonicalName = default(string), string domainValidationMethod = default(string))
+            : base(location, id, name, kind, type, tags, systemData)
         {
             FriendlyName = friendlyName;
             SubjectName = subjectName;
@@ -95,6 +99,7 @@ namespace Microsoft.Azure.Management.WebSites.Models
             KeyVaultSecretStatus = keyVaultSecretStatus;
             ServerFarmId = serverFarmId;
             CanonicalName = canonicalName;
+            DomainValidationMethod = domainValidationMethod;
             CustomInit();
         }
 
@@ -233,6 +238,12 @@ namespace Microsoft.Azure.Management.WebSites.Models
         public string CanonicalName { get; set; }
 
         /// <summary>
+        /// Gets or sets method of domain validation for free cert
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.domainValidationMethod")]
+        public string DomainValidationMethod { get; set; }
+
+        /// <summary>
         /// Validate the object.
         /// </summary>
         /// <exception cref="ValidationException">
@@ -241,10 +252,6 @@ namespace Microsoft.Azure.Management.WebSites.Models
         public override void Validate()
         {
             base.Validate();
-            if (Password == null)
-            {
-                throw new ValidationException(ValidationRules.CannotBeNull, "Password");
-            }
         }
     }
 }
