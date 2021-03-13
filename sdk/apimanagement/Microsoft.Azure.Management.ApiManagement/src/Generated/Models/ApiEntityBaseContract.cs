79cc6c635e35b0d98cb4dff796e2f9cb92f8cbdf
SDKAuto <sdkautomation@microsoft.com>
2021-03-13 01:30:24 +0000

    CodeGen from PR 13412 in Azure/azure-rest-api-specs Merge c572b8efc12db6fb1899e3233aada45655e440b4 into b81a30cfeca9873872374279e8351923326cdb04

--- sdk/apimanagement/Microsoft.Azure.Management.ApiManagement/src/Generated/Models/ApiEntityBaseContract.cs
+++ sdk/apimanagement/Microsoft.Azure.Management.ApiManagement/src/Generated/Models/ApiEntityBaseContract.cs
@@ -10,6 +10,7 @@
 
 namespace Microsoft.Azure.Management.ApiManagement.Models
 {
+    using Microsoft.Rest;
     using Newtonsoft.Json;
     using System.Linq;
 
@@ -36,7 +37,7 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
         /// <param name="subscriptionKeyParameterNames">Protocols over which
         /// API is made available.</param>
         /// <param name="apiType">Type of API. Possible values include: 'http',
-        /// 'soap'</param>
+        /// 'soap', 'websocket'</param>
         /// <param name="apiRevision">Describes the Revision of the Api. If no
         /// value is provided, default revision 1 is created</param>
         /// <param name="apiVersion">Indicates the Version identifier of the
@@ -53,7 +54,11 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
         /// ApiVersionSet.</param>
         /// <param name="subscriptionRequired">Specifies whether an API or
         /// Product subscription is required for accessing the API.</param>
-        public ApiEntityBaseContract(string description = default(string), AuthenticationSettingsContract authenticationSettings = default(AuthenticationSettingsContract), SubscriptionKeyParameterNamesContract subscriptionKeyParameterNames = default(SubscriptionKeyParameterNamesContract), string apiType = default(string), string apiRevision = default(string), string apiVersion = default(string), bool? isCurrent = default(bool?), bool? isOnline = default(bool?), string apiRevisionDescription = default(string), string apiVersionDescription = default(string), string apiVersionSetId = default(string), bool? subscriptionRequired = default(bool?))
+        /// <param name="termsOfServiceUrl"> A URL to the Terms of Service for
+        /// the API. MUST be in the format of a URL.</param>
+        /// <param name="contact">Contact information for the API.</param>
+        /// <param name="license">License information for the API.</param>
+        public ApiEntityBaseContract(string description = default(string), AuthenticationSettingsContract authenticationSettings = default(AuthenticationSettingsContract), SubscriptionKeyParameterNamesContract subscriptionKeyParameterNames = default(SubscriptionKeyParameterNamesContract), string apiType = default(string), string apiRevision = default(string), string apiVersion = default(string), bool? isCurrent = default(bool?), bool? isOnline = default(bool?), string apiRevisionDescription = default(string), string apiVersionDescription = default(string), string apiVersionSetId = default(string), bool? subscriptionRequired = default(bool?), string termsOfServiceUrl = default(string), ApiContactInformation contact = default(ApiContactInformation), ApiLicenseInformation license = default(ApiLicenseInformation))
         {
             Description = description;
             AuthenticationSettings = authenticationSettings;
@@ -67,6 +72,9 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
             ApiVersionDescription = apiVersionDescription;
             ApiVersionSetId = apiVersionSetId;
             SubscriptionRequired = subscriptionRequired;
+            TermsOfServiceUrl = termsOfServiceUrl;
+            Contact = contact;
+            License = license;
             CustomInit();
         }
 
@@ -96,7 +104,8 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
         public SubscriptionKeyParameterNamesContract SubscriptionKeyParameterNames { get; set; }
 
         /// <summary>
-        /// Gets or sets type of API. Possible values include: 'http', 'soap'
+        /// Gets or sets type of API. Possible values include: 'http', 'soap',
+        /// 'websocket'
         /// </summary>
         [JsonProperty(PropertyName = "type")]
         public string ApiType { get; set; }
@@ -152,5 +161,65 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
         [JsonProperty(PropertyName = "subscriptionRequired")]
         public bool? SubscriptionRequired { get; set; }
 
+        /// <summary>
+        /// Gets or sets  A URL to the Terms of Service for the API. MUST be in
+        /// the format of a URL.
+        /// </summary>
+        [JsonProperty(PropertyName = "termsOfServiceUrl")]
+        public string TermsOfServiceUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets contact information for the API.
+        /// </summary>
+        [JsonProperty(PropertyName = "contact")]
+        public ApiContactInformation Contact { get; set; }
+
+        /// <summary>
+        /// Gets or sets license information for the API.
+        /// </summary>
+        [JsonProperty(PropertyName = "license")]
+        public ApiLicenseInformation License { get; set; }
+
+        /// <summary>
+        /// Validate the object.
+        /// </summary>
+        /// <exception cref="ValidationException">
+        /// Thrown if validation fails
+        /// </exception>
+        public virtual void Validate()
+        {
+            if (ApiRevision != null)
+            {
+                if (ApiRevision.Length > 100)
+                {
+                    throw new ValidationException(ValidationRules.MaxLength, "ApiRevision", 100);
+                }
+                if (ApiRevision.Length < 1)
+                {
+                    throw new ValidationException(ValidationRules.MinLength, "ApiRevision", 1);
+                }
+            }
+            if (ApiVersion != null)
+            {
+                if (ApiVersion.Length > 100)
+                {
+                    throw new ValidationException(ValidationRules.MaxLength, "ApiVersion", 100);
+                }
+            }
+            if (ApiRevisionDescription != null)
+            {
+                if (ApiRevisionDescription.Length > 256)
+                {
+                    throw new ValidationException(ValidationRules.MaxLength, "ApiRevisionDescription", 256);
+                }
+            }
+            if (ApiVersionDescription != null)
+            {
+                if (ApiVersionDescription.Length > 256)
+                {
+                    throw new ValidationException(ValidationRules.MaxLength, "ApiVersionDescription", 256);
+                }
+            }
+        }
     }
 }
