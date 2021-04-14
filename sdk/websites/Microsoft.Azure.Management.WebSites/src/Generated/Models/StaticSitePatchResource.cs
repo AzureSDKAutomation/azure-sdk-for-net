99674b8bd26fa972466082016c83a417faadafdc
SDKAuto <sdkautomation@microsoft.com>
2021-04-14 07:55:34 +0000

    CodeGen from PR 13768 in Azure/azure-rest-api-specs Merge d9effa3616cf784d50adc8754e513bcbd1f86f4d into 6fa6101166a28f1fd82d6a60b8038c9af236e2be

--- sdk/websites/Microsoft.Azure.Management.WebSites/src/Generated/Models/StaticSitePatchResource.cs
+++ sdk/websites/Microsoft.Azure.Management.WebSites/src/Generated/Models/StaticSitePatchResource.cs
@@ -43,6 +43,8 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// <param name="repositoryUrl">URL for the repository of the static
         /// site.</param>
         /// <param name="branch">The target branch in the repository.</param>
+        /// <param name="provider">The provider that submitted the last
+        /// deployment to the primary environment of the static site.</param>
         /// <param name="customDomains">The custom domains associated with this
         /// static site.</param>
         /// <param name="repositoryToken">A user's github repository token.
@@ -50,15 +52,39 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// secrets.</param>
         /// <param name="buildProperties">Build properties to configure on the
         /// repository.</param>
-        public StaticSitePatchResource(string id = default(string), string name = default(string), string kind = default(string), string type = default(string), string defaultHostname = default(string), string repositoryUrl = default(string), string branch = default(string), IList<string> customDomains = default(IList<string>), string repositoryToken = default(string), StaticSiteBuildProperties buildProperties = default(StaticSiteBuildProperties))
+        /// <param name="templateProperties">Template options for generating a
+        /// new repository.</param>
+        /// <param name="privateEndpointConnections">Private endpoint
+        /// connections</param>
+        /// <param name="contentDistributionEndpoint">The content distribution
+        /// endpoint for the static site.</param>
+        /// <param name="keyVaultReferenceIdentity">Identity to use for Key
+        /// Vault Reference authentication.</param>
+        /// <param name="userProvidedFunctionApps">User provided function apps
+        /// registered with the static site</param>
+        /// <param name="stagingEnvironmentPolicy">State indicating whether
+        /// staging environments are allowed or not allowed for a static web
+        /// app. Possible values include: 'Enabled', 'Disabled'</param>
+        /// <param name="allowConfigFileUpdates">&lt;code&gt;false&lt;/code&gt;
+        /// if config file is locked for this static web app; otherwise,
+        /// &lt;code&gt;true&lt;/code&gt;.</param>
+        public StaticSitePatchResource(string id = default(string), string name = default(string), string kind = default(string), string type = default(string), string defaultHostname = default(string), string repositoryUrl = default(string), string branch = default(string), string provider = default(string), IList<string> customDomains = default(IList<string>), string repositoryToken = default(string), StaticSiteBuildProperties buildProperties = default(StaticSiteBuildProperties), StaticSiteTemplateOptions templateProperties = default(StaticSiteTemplateOptions), IList<ResponseMessageEnvelopeRemotePrivateEndpointConnection> privateEndpointConnections = default(IList<ResponseMessageEnvelopeRemotePrivateEndpointConnection>), string contentDistributionEndpoint = default(string), string keyVaultReferenceIdentity = default(string), IList<StaticSiteUserProvidedFunctionApp> userProvidedFunctionApps = default(IList<StaticSiteUserProvidedFunctionApp>), StagingEnvironmentPolicy? stagingEnvironmentPolicy = default(StagingEnvironmentPolicy?), bool? allowConfigFileUpdates = default(bool?))
             : base(id, name, kind, type)
         {
             DefaultHostname = defaultHostname;
             RepositoryUrl = repositoryUrl;
             Branch = branch;
+            Provider = provider;
             CustomDomains = customDomains;
             RepositoryToken = repositoryToken;
             BuildProperties = buildProperties;
+            TemplateProperties = templateProperties;
+            PrivateEndpointConnections = privateEndpointConnections;
+            ContentDistributionEndpoint = contentDistributionEndpoint;
+            KeyVaultReferenceIdentity = keyVaultReferenceIdentity;
+            UserProvidedFunctionApps = userProvidedFunctionApps;
+            StagingEnvironmentPolicy = stagingEnvironmentPolicy;
+            AllowConfigFileUpdates = allowConfigFileUpdates;
             CustomInit();
         }
 
@@ -86,6 +112,13 @@ namespace Microsoft.Azure.Management.WebSites.Models
         public string Branch { get; set; }
 
         /// <summary>
+        /// Gets the provider that submitted the last deployment to the primary
+        /// environment of the static site.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.provider")]
+        public string Provider { get; private set; }
+
+        /// <summary>
         /// Gets the custom domains associated with this static site.
         /// </summary>
         [JsonProperty(PropertyName = "properties.customDomains")]
@@ -103,6 +136,52 @@ namespace Microsoft.Azure.Management.WebSites.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.buildProperties")]
         public StaticSiteBuildProperties BuildProperties { get; set; }
+
+        /// <summary>
+        /// Gets or sets template options for generating a new repository.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.templateProperties")]
+        public StaticSiteTemplateOptions TemplateProperties { get; set; }
+
+        /// <summary>
+        /// Gets private endpoint connections
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.privateEndpointConnections")]
+        public IList<ResponseMessageEnvelopeRemotePrivateEndpointConnection> PrivateEndpointConnections { get; private set; }
+
+        /// <summary>
+        /// Gets the content distribution endpoint for the static site.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.contentDistributionEndpoint")]
+        public string ContentDistributionEndpoint { get; private set; }
+
+        /// <summary>
+        /// Gets identity to use for Key Vault Reference authentication.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.keyVaultReferenceIdentity")]
+        public string KeyVaultReferenceIdentity { get; private set; }
+
+        /// <summary>
+        /// Gets user provided function apps registered with the static site
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.userProvidedFunctionApps")]
+        public IList<StaticSiteUserProvidedFunctionApp> UserProvidedFunctionApps { get; private set; }
+
+        /// <summary>
+        /// Gets or sets state indicating whether staging environments are
+        /// allowed or not allowed for a static web app. Possible values
+        /// include: 'Enabled', 'Disabled'
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.stagingEnvironmentPolicy")]
+        public StagingEnvironmentPolicy? StagingEnvironmentPolicy { get; set; }
+
+        /// <summary>
+        /// Gets or sets &amp;lt;code&amp;gt;false&amp;lt;/code&amp;gt; if
+        /// config file is locked for this static web app; otherwise,
+        /// &amp;lt;code&amp;gt;true&amp;lt;/code&amp;gt;.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.allowConfigFileUpdates")]
+        public bool? AllowConfigFileUpdates { get; set; }
 
     }
 }
