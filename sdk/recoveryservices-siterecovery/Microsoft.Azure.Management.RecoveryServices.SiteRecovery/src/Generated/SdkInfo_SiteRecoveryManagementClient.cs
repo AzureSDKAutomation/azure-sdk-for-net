62a603639f5a8937b2eda805ed326419ccdf9fe7
SDKAuto <sdkautomation@microsoft.com>
2021-04-22 12:25:54 +0000

    CodeGen from PR 14077 in Azure/azure-rest-api-specs Merge b06ec799685289d3a00dce242d7293252c157543 into 4328af2265396d332b54464b62b8561ed6f44cae

--- sdk/recoveryservices-siterecovery/Microsoft.Azure.Management.RecoveryServices.SiteRecovery/src/Generated/SdkInfo_SiteRecoveryManagementClient.cs
+++ sdk/recoveryservices-siterecovery/Microsoft.Azure.Management.RecoveryServices.SiteRecovery/src/Generated/SdkInfo_SiteRecoveryManagementClient.cs
@@ -49,16 +49,5 @@ namespace Microsoft.Azure.Management.RecoveryServices.SiteRecovery
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/recoveryservicessiterecovery/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=D:\\repos\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "64e122e05a8ff184be8a6d185edfab4e9c3c237e";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
