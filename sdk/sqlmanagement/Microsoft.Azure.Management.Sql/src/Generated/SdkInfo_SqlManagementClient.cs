3d87024dadeef240ae7367bb0db940cfbc98b263
SDKAuto <sdkautomation@microsoft.com>
2021-05-17 22:35:57 +0000

    CodeGen from PR 14439 in Azure/azure-rest-api-specs Merge 40a4f80e3b90f568a656ca1d868e13c212cd79bf into f15b4f3299df66c755156aaea2c3f0c64f1840bb

--- sdk/sqlmanagement/Microsoft.Azure.Management.Sql/src/Generated/SdkInfo_SqlManagementClient.cs
+++ sdk/sqlmanagement/Microsoft.Azure.Management.Sql/src/Generated/SdkInfo_SqlManagementClient.cs
@@ -145,16 +145,5 @@ namespace Microsoft.Azure.Management.Sql
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/sql/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "7d4caa5e1c9996a25300b0b23ef426667e4a7bde";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
